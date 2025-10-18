6f4f58769f4bb1ab2da5c106949939bd6da450bf
Tobias Kohlbau <tobias@kohlbau.de>
2025-10-18 11:49:04 +0200

    chore: move tools around
    Move tools around and cleanup build script.

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.Build) void {
     const embed_files = b.addExecutable(.{
         .name = "embed_fs",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("build/embed_fs.zig"),
+            .root_source_file = b.path("tools/embed_fs.zig"),
             .target = b.graph.host,
         }),
     });
@@ -31,13 +31,9 @@ pub fn build(b: *std.Build) void {
     const mqtt = b.dependency("mqtt", .{});
     const ftp = b.dependency("ftp", .{});
 
-    const http_module = http.module("http");
-    const mqtt_module = mqtt.module("mqtt");
-    const ftp_module = ftp.module("ftp");
-
-    exe.root_module.addImport("http", http_module);
-    exe.root_module.addImport("mqtt", mqtt_module);
-    exe.root_module.addImport("ftp", ftp_module);
+    exe.root_module.addImport("http", http.module("http"));
+    exe.root_module.addImport("mqtt", mqtt.module("mqtt"));
+    exe.root_module.addImport("ftp", ftp.module("ftp"));
 
     exe.root_module.addAnonymousImport("ui", .{
         .root_source_file = output,
@@ -52,31 +48,4 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
-
-    const exe_docs = b.addInstallDirectory(.{
-        .source_dir = exe.getEmittedDocs(),
-        .install_dir = .prefix,
-        .install_subdir = "docs",
-    });
-
-    const docs_step = b.step("docs", "Copy documentation to prefix path");
-    docs_step.dependOn(&exe_docs.step);
-    docs_step.dependOn(&(moduleDocs(b, http_module, "httpz").step));
-    docs_step.dependOn(&(moduleDocs(b, mqtt_module, "mqttz").step));
-    docs_step.dependOn(&(moduleDocs(b, ftp_module, "mqttz").step));
-}
-
-fn moduleDocs(b: *std.Build, module: *std.Build.Module, name: []const u8) *std.Build.Step.InstallDir {
-    const lib = b.addLibrary(.{
-        .name = name,
-        .root_module = module,
-    });
-
-    const install_dir = b.addInstallDirectory(.{
-        .source_dir = lib.getEmittedDocs(),
-        .install_dir = .prefix,
-        .install_subdir = "docs",
-    });
-
-    return install_dir;
 }
